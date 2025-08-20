kaffuchino233/hardware_oplus

VibratorExt: Add support for corvette
Change-Id: I0f14ac3a7a36e86ed62940c4017f64cebafaa8a8

aidl/vibratorExt/VibratorExt.cpp
@@ -5,6 +5,7 @@
 
 #include "include/StubVibrator.h"
 #include "include/VibratorExt.h"
+#include "include/device/corvette_0916t.h"
 #include "include/device/dodge_0916t.h"
 #include "include/device/kona_0815.h"
 #include "include/device/lahaina_0809.h"
@@ -26,6 +27,16 @@ VibratorExt::VibratorExt() : mVibrator(nullptr) {
     std::string deviceName = GetProperty("ro.nameless.device", "");
     if (DEBUG_ENABLED) {
         ALOGD("device name: %s", deviceName.c_str());
+    }
+
+    // OnePlus Ace 3 Pro
+    if (deviceName == "corvette") {
+        mVibrator = new corvette_0916t_vibrator();
+        SetProperty("sys.nameless.feature.vibrator.external_control", "true");
+        SetProperty("sys.nameless.feature.vibrator.richtap", "true");
+        SetProperty("sys.nameless.feature.vibrator.richtap_dev", "/dev/awinic_haptic");
+        SetProperty("sys.nameless.feature.vibrator.richtap_config", "/odm/etc/aac_richtap.config");
+        goto END;
     }
 
     // OnePlus 13

aidl/vibratorExt/include/Constant.h
@@ -62,7 +62,7 @@ static const LevelRangeConfig UNIFIED_LEVEL_RANGE_CONFIG = {
     }
 };
 
-static const LevelRangeConfig LEVEL_RANGE_CONFIG_OP11_12_13 = {
+static const LevelRangeConfig LEVEL_RANGE_CONFIG_OP11_12_13_ACE3P = {
     .alarmCallLevelRange = {
         .maxLevel = 5,
         .defaultLevel = 4

LucasBlackLu/android_hardware_oplus

aidl/vibratorExt/VibratorExt.cpp
@@ -6,6 +6,7 @@
 #include "include/StubVibrator.h"
 #include "include/VibratorExt.h"
 #include "include/device/aston_0809.h"
+#include "include/device/corvette_0916t.h"
 #include "include/device/dodge_0916t.h"
 #include "include/device/kona_0815.h"
 #include "include/device/lahaina_0809.h"
@@ -41,6 +42,15 @@ VibratorExt::VibratorExt() : mVibrator(nullptr) {
     // OnePlus 12
     if (deviceName == "waffle") {
         mVibrator = new waffle_0916t_vibrator();
+        SetProperty("sys.sun.feature.vibrator.external_control", "true");
+        SetProperty("sys.sun.feature.vibrator.richtap", "true");
+        SetProperty("sys.sun.feature.vibrator.richtap_dev", "/dev/awinic_haptic");
+        SetProperty("sys.sun.feature.vibrator.richtap_config", "/odm/etc/aac_richtap.config");
+        goto END;
+    }
+    // OnePlus Ace 3 Pro
+    if (deviceName == "corvette") {
+        mVibrator = new corvette_0916t_vibrator();
         SetProperty("sys.sun.feature.vibrator.external_control", "true");
         SetProperty("sys.sun.feature.vibrator.richtap", "true");
         SetProperty("sys.sun.feature.vibrator.richtap_dev", "/dev/awinic_haptic");

aidl/vibratorExt/include/Constant.h
@@ -62,7 +62,7 @@ static const LevelRangeConfig UNIFIED_LEVEL_RANGE_CONFIG = {
     }
 };
 
-static const LevelRangeConfig LEVEL_RANGE_CONFIG_OP11_12_12R = {
+static const LevelRangeConfig LEVEL_RANGE_CONFIG_OP11_12_12R_ACE3P = {
     .alarmCallLevelRange = {
         .maxLevel = 5,
         .defaultLevel = 4